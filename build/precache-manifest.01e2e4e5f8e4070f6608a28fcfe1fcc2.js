fc160ac35a5698f935e814aa1392165d3fb1a52f
deho9147 <deho9147@colorado.edu>
2020-01-15 20:54:00 -0700

    ready to launch simple site

---
+++ build/precache-manifest.01e2e4e5f8e4070f6608a28fcfe1fcc2.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a431f5b52d1d188dd5ab47462721ed88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24feab6b451865a17157",
+    "url": "/static/css/main.d6658b5e.chunk.css"
+  },
+  {
+    "revision": "10e48b815020e36aba36",
+    "url": "/static/js/2.8c4377b3.chunk.js"
+  },
+  {
+    "revision": "a73d63b4ffb53c48db64df65b25226bb",
+    "url": "/static/js/2.8c4377b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24feab6b451865a17157",
+    "url": "/static/js/main.f124b7fa.chunk.js"
+  },
+  {
+    "revision": "8d97ff905b294249d064",
+    "url": "/static/js/runtime-main.d01be472.js"
+  },
+  {
+    "revision": "2bcbaf33b695586f25d8f70a444ee0a7",
+    "url": "/static/media/1976.2bcbaf33.csv"
+  },
+  {
+    "revision": "97b41221ffbd6f5accc5672f675b7e6a",
+    "url": "/static/media/1976_compress.97b41221.svg"
+  },
+  {
+    "revision": "f5ee13eadbbb81027ba14c46fdf38e7e",
+    "url": "/static/media/1980.f5ee13ea.csv"
+  },
+  {
+    "revision": "1c4b5d983f99789c95a2f55ae153a928",
+    "url": "/static/media/1980_compress.1c4b5d98.svg"
+  },
+  {
+    "revision": "ca56aebc765624595118ad95896300c4",
+    "url": "/static/media/1984.ca56aebc.csv"
+  },
+  {
+    "revision": "19afea37839ae91596e68dfa8c907f68",
+    "url": "/static/media/1984_compress.19afea37.svg"
+  },
+  {
+    "revision": "50b9a789469c23b7a21c76cebc981585",
+    "url": "/static/media/1988.50b9a789.csv"
+  },
+  {
+    "revision": "ac65390cc39984e9d0b1af92b019d6f8",
+    "url": "/static/media/1988_compress.ac65390c.svg"
+  },
+  {
+    "revision": "566c354761a130853f5c16a313796fd7",
+    "url": "/static/media/1992.566c3547.csv"
+  },
+  {
+    "revision": "2eee47cbbcd08abf446a8a9c676258ab",
+    "url": "/static/media/1992_compress.2eee47cb.svg"
+  },
+  {
+    "revision": "d2d0d0bed1ba135d67cb566a1d6a3272",
+    "url": "/static/media/1996.d2d0d0be.csv"
+  },
+  {
+    "revision": "cd27d055ca63e975b6623b1dec46a38d",
+    "url": "/static/media/1996_compress.cd27d055.svg"
+  },
+  {
+    "revision": "98d25d4439d84c5adf519490126add83",
+    "url": "/static/media/2000.98d25d44.csv"
+  },
+  {
+    "revision": "a9eee9845ef44351fde6e47cfe83089f",
+    "url": "/static/media/2000_compress.a9eee984.svg"
+  },
+  {
+    "revision": "4306ecee97b066635ec332a2285a77b9",
+    "url": "/static/media/2004.4306ecee.csv"
+  },
+  {
+    "revision": "96594ef22b9a4e50fba358f49dfa2a94",
+    "url": "/static/media/2004_compress.96594ef2.svg"
+  },
+  {
+    "revision": "44fb6511f2ed8af996560b04cda3cdc6",
+    "url": "/static/media/2008.44fb6511.csv"
+  },
+  {
+    "revision": "afe3aa634833d184d538217074a9457a",
+    "url": "/static/media/2008_compress.afe3aa63.svg"
+  },
+  {
+    "revision": "4afa0a1733810c3b331146469f50e197",
+    "url": "/static/media/2012.4afa0a17.csv"
+  },
+  {
+    "revision": "4c72e5088cfa98265f493f90607c29c3",
+    "url": "/static/media/2012_compress.4c72e508.svg"
+  },
+  {
+    "revision": "c3cc60c73baa03cfbef4cdaa97161dcf",
+    "url": "/static/media/2016.c3cc60c7.csv"
+  },
+  {
+    "revision": "06ffcd4fce9b6a2e70d9ed6ff67927cc",
+    "url": "/static/media/2016_compress.06ffcd4f.svg"
+  },
+  {
+    "revision": "d2b36b53b34ab93b7eac04e801d54afc",
+    "url": "/static/media/DerekHolland_resume.d2b36b53.pdf"
+  },
+  {
+    "revision": "fe1b57d42b9c00a3efeffb4443107553",
+    "url": "/static/media/Headshot.fe1b57d4.png"
+  },
+  {
+    "revision": "0727a93c4dc97974e27c5bb36df10357",
+    "url": "/static/media/skij_front.0727a93c.png"
+  },
+  {
+    "revision": "def8b9e7a641219be1d7d4b154c73f06",
+    "url": "/static/media/skij_options.def8b9e7.png"
+  }
+]);
